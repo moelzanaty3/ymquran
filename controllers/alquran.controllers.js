9a831672cf124ae51957b5c0c585e478dbcbe5a7
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-03-01 13:27:50 +0200

    fix long tweets

--- controllers/alquran.controllers.js
+++ controllers/alquran.controllers.js
@@ -3,15 +3,20 @@ const axios = require('axios')
 const utils = require('../utils')
 
 const getAyah = async () => {
-  const ayahNumber = utils.randomNumbersBetween(1, 6236)
   try {
-    const response = await axios(`https://api.alquran.cloud/v1/ayah/${ayahNumber}`)
-    const data = response.data.data ? response.data.data : {}
+    let ayahText = ''
+    let ayahData = {}
     let tweet
-    if (data && data.text) {
-      const ayahText = data.text.slice(0, 255)
-      const ayahSurah = data.surah.name
-      const ayahNumber = data.surah.numberOfAyahs
+    while (true) {
+      const ayahNumber = utils.randomNumbersBetween(1, 6236)
+      const response = await axios(`https://api.alquran.cloud/v1/ayah/${ayahNumber}`)
+      ayahData = response.data.data ? response.data.data : {}
+      if (ayahData.text.length <= 250) break
+    }
+    if (ayahData && ayahData.text) {
+      ayahText = ayahData.text
+      const ayahSurah = ayahData.surah.name
+      const ayahNumber = ayahData.surah.numberOfAyahs
       tweet = `{${ayahText}} - ${ayahSurah} (${ayahNumber})`
     }
     console.log(tweet)
@@ -20,8 +25,6 @@ const getAyah = async () => {
     console.error(err)
   }
 }
-
-getAyah()
 
 module.exports = {
   getAyah
